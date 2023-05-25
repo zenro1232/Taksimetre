c498f90c55e2e6e5d35d886107ef42685c0c3dec
zenro1232 <126719730+zenro1232@users.noreply.github.com>
2023-05-25 17:59:45 +0300

    Taksimetre.java

--- src/Taksimetre.java
+++ src/Taksimetre.java
@@ -7,14 +7,14 @@ public class Taksimetre
         Scanner input = new Scanner(System.in);
         int km;
         double perkm = 2.20, total = 10, min = 20;
-        System.out.print("Lütfen KM cinsinden giriniz");
+        System.out.print("Lütfen KM cinsinden giriniz  :");
         km = input.nextInt();
 
         total += (perkm * km);
 
         total = total < 20 ? 20 : total;
 
-System.out.print(total);
+        System.out.print(total);
 
 
 
